e7ffad918f2d610f9eec8eb940bf963b03f72d36
Yadidiah Kanaparthi <94169481+YADIDidiah24@users.noreply.github.com>
2024-05-17 01:36:48 +0400

    Delete Web_Frameworks/django-marketplace directory

--- Web_Frameworks/django-marketplace/item/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from core.models import Item
-
-class NewItemForm(forms.ModelForm):
-    class Meta:
-        model = Item
-        fields = ('category','name','description','price','image')
-        
